9300aa0a91bd9b080c551276a73cc67c4d94b62f
Lucas Carlos <lucas2carlos@gmail.com>
2024-05-16 14:25:37 -0400

    adding loop

--- obj/Debug/net8.0/PetsApplicationChallenge.AssemblyInfo.cs
+++ obj/Debug/net8.0/PetsApplicationChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetsApplicationChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c47a7e5088fe555e5bcf238cf3081037c7a2c53e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14bf267b6d92b66419607ed407cad422c582199e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetsApplicationChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetsApplicationChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
