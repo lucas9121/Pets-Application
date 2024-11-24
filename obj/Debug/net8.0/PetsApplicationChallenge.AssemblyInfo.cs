753ccf479ad2a042c24ad2a74fbf1a6e0ab0a53c
Lucas Carlos <lucas2carlos@gmail.com>
2024-11-24 18:22:49 -0500

    fixing case 2

--- obj/Debug/net8.0/PetsApplicationChallenge.AssemblyInfo.cs
+++ obj/Debug/net8.0/PetsApplicationChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetsApplicationChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+531302ecb82ef6998c7751377bd5b070b3946af4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d649ce6a38f16ba787d6f92e273e3db197fac754")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetsApplicationChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetsApplicationChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
